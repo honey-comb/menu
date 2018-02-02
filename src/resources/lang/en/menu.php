53eaac5d1fb043043e3db9dd54b122fd217509bd
Jevgenij Dmitrijev <jd@interactivesolutions.lt>
2018-02-02 16:02:21 +0200

    Added translations

---
+++ src/resources/lang/en/menu.php
@@ -0,0 +1,14 @@
+<?php
+return [
+    "page_title" => "Menu",
+    "language_code" => "Language code",
+    "type_id" => "Type",
+    "parent_id" => "Parent",
+    "label" => "Label",
+    "type" => "Type",
+    "sequence" => "Sequence",
+    "icon" => "Icon",
+    "url" => "Url",
+    "url_text" => "Url text",
+    "url_type" => "Url type",
+];
