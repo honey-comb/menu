542f3d3ac2df184f5a3b344764c29fdceea299de
Jevgenij Dmitrijev <jd@interactivesolutions.lt>
2018-02-02 20:50:46 +0200

    sequence->default(0)

--- src/database/migrations/2018_01_30_054025_create_hc_menu_table.php
+++ src/database/migrations/2018_01_30_054025_create_hc_menu_table.php
@@ -3,34 +3,34 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 
-class CreateHcMenuTable extends Migration {
+class CreateHcMenuTable extends Migration
+{
 
-	/**
-	 * Run the migrations.
-	 *
-	 * @return void
-	 */
-	public function up()
-	{
-		Schema::create('hc_menu', function(Blueprint $table)
-		{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('hc_menu', function(Blueprint $table) {
             $table->increments('count');
             $table->uuid('id')->unique();
             $table->datetime('created_at')->default(DB::raw('CURRENT_TIMESTAMP'));
             $table->datetime('updated_at')->default(DB::raw('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP'));
             $table->datetime('deleted_at')->nullable();
 
-			$table->uuid('language_code')->index();
-			$table->uuid('type_id')->index();
-			$table->uuid('parent_id')->index()->nullable();
+            $table->uuid('language_code')->index();
+            $table->uuid('type_id')->index();
+            $table->uuid('parent_id')->index()->nullable();
 
-			$table->string('label');
-			$table->enum('target', array('url','page','parent'));
-			$table->integer('sequence');
-			$table->string('icon')->nullable();
-			$table->string('url', 2000)->nullable();
-			$table->string('url_text')->nullable();
-			$table->enum('url_target', array('_self','_blank'))->nullable();
+            $table->string('label');
+            $table->enum('target', ['url', 'page']);
+            $table->integer('sequence')->default(0);
+            $table->string('icon')->nullable();
+            $table->string('url', 2000)->nullable();
+            $table->string('url_text')->nullable();
+            $table->enum('url_target', ['_self', '_blank'])->nullable();
 
             $table->foreign('type_id')->references('id')
                 ->on('hc_menu_type')
@@ -41,18 +41,18 @@ class CreateHcMenuTable extends Migration {
                 ->on('hc_languages')
                 ->onUpdate('NO ACTION')
                 ->onDelete('NO ACTION');
-		});
-	}
+        });
+    }
 
 
-	/**
-	 * Reverse the migrations.
-	 *
-	 * @return void
-	 */
-	public function down()
-	{
-		Schema::drop('hc_menu');
-	}
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('hc_menu');
+    }
 
 }
